f185eca9f51d6aff79d68be28f2f665282da8c72
Jochen Weiss <mail2jogi@googlemail.com>
2020-10-07 18:05:28 +0200

    changed to OOAD done

--- src/Python-OpenWeather.py
+++ src/Python-OpenWeather.py
@@ -41,117 +41,103 @@ class OpenWeatherReceiver():
         self.locationID = locationID
         self.apikey = apikey
 
-    
-locationID = 2911964
-apikey = '4018963b8a12ea4aafa4b61cebcb9f8a'
-serviceUrl = "http://api.openweathermap.org/data/2.5/weather?"
+    def retrievWeatherData(self):
+        self.serviceUrl = "http://api.openweathermap.org/data/2.5/weather?"
+        url = self.serviceUrl + urllib.parse.urlencode({'id': self.locationID, 'APPID': self.apikey})
+        urlRead = urllib.request.urlopen(url).read()
+        dataJSON = json.loads(urlRead)
 
-#Python 3
-url = serviceUrl + urllib.parse.urlencode({'id': locationID, 'APPID': apikey})
-urlRead = urllib.request.urlopen(url).read()
+        # coord #
+        self.coord__lon = float(dataJSON['coord']['lon'])
+        self.coord__lon = float(dataJSON['coord']['lat'])
+        # weaher
+        self.weather_id = dataJSON['weather'][0]['id']
+        self.weather_main = dataJSON['weather'][0]['main']
+        self.weather_description = dataJSON['weather'][0]['description']
+        self.weather_icon = dataJSON['weather'][0]['icon']
 
-#Python 2
-#url = serviceUrl + urllib.urlencode({'id': locationID, 'APPID': apikey})
-#urlRead = urllib.urlopen(url).read()
-# API response info https://openweathermap.org/current
-dataJSON = json.loads(urlRead)
+        # base #
+        self.base = dataJSON['base']
 
-# coord #
-coord__lon = float(dataJSON['coord']['lon'])
-coord__lon = float(dataJSON['coord']['lat'])
-# weaher
-weather_id = dataJSON['weather'][0]['id']
-weather_main = dataJSON['weather'][0]['main']
-weather_description = dataJSON['weather'][0]['description']
-weather_icon = dataJSON['weather'][0]['icon']
+        # main #
+        self.main_temp = float(dataJSON['main']['temp'])
+        self.main_pressure = float(dataJSON['main']['pressure'])
+        self.main_humidity = float(dataJSON['main']['humidity'])
+        self.main_temp_min = float(dataJSON['main']['temp_min'])
+        self.main_temp_max = float(dataJSON['main']['temp_max'])
 
-# base #
-base = dataJSON['base']
+        # wind #
+        self.wind = dataJSON['wind']
+        self.wind_speed = float(dataJSON['wind']['speed'])
+        self.wind_deg = float(dataJSON['wind']['deg'])
+        if 'gust' in self.wind:
+            self.wind_gust = float(dataJSON['wind']['gust'])
 
-# main #
-main_temp = float(dataJSON['main']['temp'])
-main_pressure = float(dataJSON['main']['pressure'])
-main_humidity = float(dataJSON['main']['humidity'])
-main_temp_min = float(dataJSON['main']['temp_min'])
-main_temp_max = float(dataJSON['main']['temp_max'])
+        # clouds #
+        self.clouds_all = dataJSON['clouds']['all']
 
-# wind #
-wind = dataJSON['wind']
-wind_speed = float(dataJSON['wind']['speed'])
-wind_deg = float(dataJSON['wind']['deg'])
-if 'gust' in wind:
-    wind_gust = float(dataJSON['wind']['gust'])
+        # dt#
+        self.dt = float(dataJSON["dt"])
 
-# clouds #
-clouds_all = dataJSON['clouds']['all']
+        # sys #
+        self.sys_type = dataJSON['sys']['type']
+        self.sys_id = dataJSON['sys']['id']
+        # sys_message = dataJSON['sys']['message'] //KeyError 'message'
+        self.sys_country = dataJSON['sys']['country']
+        self.sys_sunrise = dataJSON['sys']['sunrise']
+        self.sys_sunset = dataJSON['sys']['sunset']
+        self.sys_timezone = dataJSON['timezone']
 
-# dt#
-dt = float(dataJSON["dt"])
+        self.dt = float(dataJSON["dt"])
+        self.dt_local = time.time()
+        self.main = dataJSON
+        self.temp = float(dataJSON['main']['temp']) - 273.0
+        self.tempMax = float(dataJSON['main']['temp_max']) - 273.0
+        self.tempMin = float(dataJSON['main']['temp_min']) - 273.0
+        self.tempFeelsLike = float(dataJSON['main']['feels_like']) - 273.0
+        self.humidity = int(dataJSON['main']['humidity'])
+        self.pressure = int(dataJSON['main']['pressure'])
+        self.wind = dataJSON['wind']
+        self.name = dataJSON['name']
+        self.windSpeed = float(dataJSON['wind']['speed'])
+        self.windDeg = float(dataJSON['wind']['deg'])
+        self.condition = dataJSON['weather'][0]['description']
+        self.clouds = int(dataJSON['clouds']['all'])
+        self.visibility = int(dataJSON['visibility'])
 
-# sys #
-sys_type = dataJSON['sys']['type']
-sys_id = dataJSON['sys']['id']
-# sys_message = dataJSON['sys']['message'] //KeyError 'message'
-sys_country = dataJSON['sys']['country']
-sys_sunrise = dataJSON['sys']['sunrise']
-sys_sunset = dataJSON['sys']['sunset']
-sys_timezone = dataJSON['timezone']
-sys_id = dataJSON['sys']['id']
-#sys_name = dataJSON['sys']['name']
-#sys_cod = dataJSON['sys']['cod']
+    def printWeatherData(self):
+        print("*******************")
+        print("--Weather Summary--")
+        print("*******************")
+        print("Name "+self.name)
+        print("base "+self.base)
+        print("dt "+str(self.dt))
+        print("GMT "+ str(time.asctime(time.gmtime(self.dt))))
+        print("Timezone "+str(self.sys_timezone))
+        print("LOCAL " + time.ctime(self.dt))
+        print("CURRENT "+ time.ctime(self.dt_local))
+        print("Sunrise GMT "+ str(time.asctime(time.gmtime(self.sys_sunrise))))
+        print("Sunset GMT "+ str(time.asctime(time.gmtime(self.sys_sunset))))
 
-# conn = sqlite3.connect("openweather.db")
-# c = conn.cursor()
-# my_data = (dt, main_temp, main_pressure, main_humidity, main_temp_min, main_temp_max)
-# c.execute('INSERT INTO main VALUES (? ? ? ? ? ?)', my_data)
-# conn.commit()
+        print("Current Temperature: %.2f C" % self.temp)
+        print("Maximum Temperature: %.2f C" % self.tempMax)
+        print("Minimum Temperature: %.2f C" % self.tempMin)
+        print("Feels Like Temperatur: %.2f C" % self.tempFeelsLike)
+        print("Pressure: %d hpa" % self.pressure)
+        print("Humidity: %d %%" % self.humidity)
+        if 'gust' in self.wind:
+            self.windGust = float(dataJSON['wind']['gust'])
+            print("Wind Gust:%s m/s" % self.windGust)
+        else:
+            print("Wind Gust: Data not available")
 
-##########################
-print(dataJSON)
-dt = float(dataJSON["dt"])
-dt_local = time.time()
-main = dataJSON
-temp = float(dataJSON['main']['temp']) - 273.0
-tempMax = float(dataJSON['main']['temp_max']) - 273.0
-tempMin = float(dataJSON['main']['temp_min']) - 273.0
-tempFeelsLike = float(dataJSON['main']['feels_like']) - 273.0
-humidity = int(dataJSON['main']['humidity'])
-pressure = int(dataJSON['main']['pressure'])
-wind = dataJSON['wind']
-name = dataJSON['name']
-windSpeed = float(dataJSON['wind']['speed'])
-windDeg = float(dataJSON['wind']['deg'])
-condition = dataJSON['weather'][0]['description']
-clouds = int(dataJSON['clouds']['all'])
-visibility = int(dataJSON['visibility'])
-print("urlRead "+ urlRead)
-print("*******************")
-print("--Weather Summary--")
-print("*******************")
-print("Name "+name)
-print("base "+base)
-print("dt "+str(dt))
-print("GMT "+ str(time.asctime(time.gmtime(dt))))
-print("Timezone "+str(timezone))
-print("LOCAL " + time.ctime(dt))
-print("CURRENT "+ time.ctime(dt_local))
-print("Sunrise GMT "+ str(time.asctime(time.gmtime(sys_sunrise))))
-print("Sunset GMT "+ str(time.asctime(time.gmtime(sys_sunset))))
+        print("Wind Speed: %.2f m/s " % self.windSpeed)
+        print("Wind Deg: %.2f Degree" % self.windDeg)
+        print("Clouds: %d %%" % self.clouds)
+        print("Condition: %s" % self.condition)
+        print("visibility: %s m" % str(self.visibility))
 
-print("Current Temperature: %.2f C" % temp)
-print("Maximum Temperature: %.2f C" % tempMax)
-print("Minimum Temperature: %.2f C" % tempMin)
-print("Feels Like Temperatur: %.2f C" % tempFeelsLike)
-print("Pressure: %d hpa" % pressure)
-print("Humidity: %d %%" % humidity)
-if 'gust' in wind:
-    windGust = float(dataJSON['wind']['gust'])
-    print("Wind Gust:%s m/s" % windGust)
-else:
-    print("Wind Gust: Data not available")
 
-print("Wind Speed: %.2f m/s " % windSpeed)
-print("Wind Deg: %.2f Degree" % windDeg)
-print("Clouds: %d %%" % clouds)
-print("Condition: %s" % condition)
-print("visibility: %s m" % str(visibility))
+fw = OpenWeatherReceiver(2911964, '4018963b8a12ea4aafa4b61cebcb9f8a')
+fw.retrievWeatherData()
+fw.printWeatherData()
